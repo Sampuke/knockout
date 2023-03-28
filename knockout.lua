3c7a51588049977a663bfaf573fa6cd4b801eaac
Sampuke <99171091+Sampuke@users.noreply.github.com>
2023-03-28 10:08:39 +0300

    Update knockout.lua

--- knockout.lua
+++ knockout.lua
@@ -3,53 +3,91 @@ local knockedOut = false
 local count = 60
 
 Citizen.CreateThread(function()
-    if Config.QBCore == true then
-		while Framework == nil do
+    if Config.core == "qb" then
+        while Framework == nil do
             Framework = exports['qb-core']:GetCoreObject()
             Citizen.Wait(4)
         end
-	end
+    elseif Config.core == "esx" then
+        while Framework == nil do
+            Framework = exports["es_extended"]:getSharedObject()
+            Citizen.Wait(4)
+        end
+    elseif Config.core == false then
+			
+	else
+        print("You didnt configure esx or qbcore in config, if your using custom core edit knockout.lua and change core object!!!!")
+        print("You didnt configure esx or qbcore in config, if your using custom core edit knockout.lua and change core object!!!!")
+        print("You didnt configure esx or qbcore in config, if your using custom core edit knockout.lua and change core object!!!!")
+        print("You didnt configure esx or qbcore in config, if your using custom core edit knockout.lua and change core object!!!!")
+        print("You didnt configure esx or qbcore in config, if your using custom core edit knockout.lua and change core object!!!!")
+        print("You didnt configure esx or qbcore in config, if your using custom core edit knockout.lua and change core object!!!!")
+        print("You didnt configure esx or qbcore in config, if your using custom core edit knockout.lua and change core object!!!!")
+        print("You didnt configure esx or qbcore in config, if your using custom core edit knockout.lua and change core object!!!!")
+        print("You didnt configure esx or qbcore in config, if your using custom core edit knockout.lua and change core object!!!!")
+        print("You didnt configure esx or qbcore in config, if your using custom core edit knockout.lua and change core object!!!!")
+        --[[ while Framework == nil do
+            Framework = exports["es_extended"]:getSharedObject()
+            Citizen.Wait(4)
+        end ]]
+    end
 end)
 
 Citizen.CreateThread(function()
     while true do
         Wait(1)
-        local myPed = GetPlayerPed(-1)
+        local Ped = PlayerPedId()
         -- With melee weapon or unarmed only
-        if IsPedInMeleeCombat(myPed) then
+        if IsPedInMeleeCombat(Ped) then
             -- Without any kind of weapon {UNARMED ONLY}
-            if (HasPedBeenDamagedByWeapon(myPed, GetHashKey("WEAPON_UNARMED"), 0) )then
+            if (HasPedBeenDamagedByWeapon(Ped, GetHashKey("WEAPON_UNARMED"), 0) )then
                 -- Health to be knocked out
-                if GetEntityHealth(myPed) < 145 then
-                    SetPlayerInvincible(PlayerId(), false)
-                    -- Position taken by your Ped
-                    SetPedToRagdoll(myPed, 1000, 1000, 0, 0, 0, 0)
-                    --  Effect 
-                    ShakeGameplayCam('LARGE_EXPLOSION_SHAKE', 2.5)
-                    -- Time to wait
-                    wait = Config.knockoutDurantion
-                    if Config.QBCore == true then
-                        Framework.Functions.Progressbar("random_task", "You are knocked out", Config.progressbarDurantion, false, true, {
-                            disableMovement = false,
-                            disableCarMovement = false,
-                            disableMouse = false,
-                            disableCombat = true,
-                        }, {
-                            animDict = "mp_suicide",
-                            anim = "pill",
-                            flags = 49,
-                        }, {}, {}, function()
-                            -- Done
-                            StopAnimTask(PlayerPedId(), "mp_suicide", "pill", 1.0)
-                        end, function() -- Cancel
-                            StopAnimTask(PlayerPedId(), "mp_suicide", "pill", 1.0)
-                        end)
-                    else
-                        exports['progressBars']:startUI(Config.progressbarDurantion, "You are knocked out")
-                    end
-                    knockedOut = true
-                    -- Health after knockout preferably dont make it more than 150 (50 %) because people will abuse with it {No need to go to hospital or so}
-                    SetEntityHealth(myPed, 200)
+                if GetEntityHealth(Ped) < 145 then
+                    local math = math.random(1, Config.chance)
+                    if math == 1 then
+			    SetPlayerInvincible(PlayerId(), false)
+			    -- Position taken by your Ped
+			    SetPedToRagdoll(Ped, 1000, 1000, 0, 0, 0, 0)
+			    --  Effect 
+			    ShakeGameplayCam('LARGE_EXPLOSION_SHAKE', 2.5)
+			    -- Time to wait
+			    wait = Config.knockoutDurantion
+			    if Config.core == 'qb' then
+				Framework.Functions.Progressbar("random_task", "You are knocked out", Config.progressbarDurantion, false, true, {
+				    disableMovement = false,
+				    disableCarMovement = false,
+				    disableMouse = false,
+				    disableCombat = true,
+				}, {
+				    animDict = "mp_suicide",
+				    anim = "pill",
+				    flags = 49,
+				}, {}, {}, function()
+				    -- Done
+				    StopAnimTask(PlayerPedId(), "mp_suicide", "pill", 1.0)
+				end, function() -- Cancel
+				    StopAnimTask(PlayerPedId(), "mp_suicide", "pill", 1.0)
+				end)
+			    elseif Config.core == 'esx' then
+								    ESX.Progressbar("test", 25000,{
+									FreezePlayer = false, 
+									animation ={
+									    type = "anim",
+									    dict = "mp_suicide", 
+									    lib ="pill" 
+									}, 
+									onFinish = function()
+									--Code here
+								    end})
+			    else
+								
+        			print("You need to configure your own progressbar")
+				exports['progressBars']:startUI(Config.progressbarDurantion, "You are knocked out")
+			    end
+			    knockedOut = true
+			    -- Health after knockout preferably dont make it more than 150 (50 %) because people will abuse with it {No need to go to hospital or so}
+			    SetEntityHealth(Ped, 100)
+                    end	
                 end
             end
         end
